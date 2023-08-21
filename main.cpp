2b7289abba6c6030c17ec6afdff2390b6742113e
Prathamgrao <138456172+Prathamgrao@users.noreply.github.com>
2023-08-21 14:15:21 +0530

    Add files via upload
    bfs

--- main.cpp
+++ main.cpp
@@ -1,68 +1,152 @@
-#include<iostream>
-#include<climits>
+// C++ program to find all the reachable nodes
+// for every node present in arr[0..n-1].
+#include <bits/stdc++.h>
 using namespace std;
 
-// this method returns a minimum distance for the
-// vertex which is not included in Tset.
-int minimumDist(int dist[], bool Tset[])
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
 {
-	int min=INT_MAX,index;
+public:
+	int V; // No. of vertices
 
-	for(int i=0;i<6;i++)
-	{
-		if(Tset[i]==false && dist[i]<=min)
-		{
-			min=dist[i];
-			index=i;
-		}
-	}
-	return index;
+	// Pointer to an array containing adjacency lists
+	list<int> *adj;
+
+	Graph(int ); // Constructor
+
+	void addEdge(int, int);
+
+	vector<int> BFS(int, int, int []);
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V+1];
 }
 
-void Dijkstra(int graph[6][6],int src) // adjacency matrix used is 6x6
+void Graph::addEdge(int u, int v)
 {
-	int dist[6]; // integer array to calculate minimum distance for each node.
-	bool Tset[6];// boolean array to mark visted/unvisted for each node.
+	adj[u].push_back(v); // Add w to v’s list.
+	adj[v].push_back(u); // Add v to w’s list.
+}
 
-	// set the nodes with infinity distance
-	// except for the initial node and mark
-	// them unvisited.
-	for(int i = 0; i<6; i++)
+vector<int> Graph::BFS(int componentNum, int src,
+									int visited[])
+{
+	// Mark all the vertices as not visited
+	// Create a queue for BFS
+	queue<int> queue;
+
+	queue.push(src);
+
+	// Assign Component Number
+	visited[src] = componentNum;
+
+	// Vector to store all the reachable nodes from 'src'
+	vector<int> reachableNodes;
+
+	while(!queue.empty())
 	{
-		dist[i] = INT_MAX;
-		Tset[i] = false;
-	}
+		// Dequeue a vertex from queue
+		int u = queue.front();
+		queue.pop();
 
-	dist[src] = 0;   // Source vertex distance is set to zero.
+		reachableNodes.push_back(u);
 
-	for(int i = 0; i<6; i++)
-	{
-		int m=minimumDist(dist,Tset); // vertex not yet included.
-		Tset[m]=true;// m with minimum distance included in Tset.
-		for(int i = 0; i<6; i++)
+		// Get all adjacent vertices of the dequeued
+		// vertex u. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (auto itr = adj[u].begin();
+				itr != adj[u].end(); itr++)
 		{
-			// Updating the minimum distance for the particular node.
-			if(!Tset[i] && graph[m][i] && dist[m]!=INT_MAX && dist[m]+graph[m][i]<dist[i])
-				dist[i]=dist[m]+graph[m][i];
+			if (!visited[*itr])
+			{
+				// Assign Component Number to all the
+				// reachable nodes
+				visited[*itr] = componentNum;
+				queue.push(*itr);
+			}
 		}
 	}
-	cout<<"Vertex\t\tDistance from source"<<endl;
-	for(int i = 0; i<6; i++)
-	{ //Printing
-		char str=65+i; // Ascii values for pritning A,B,C..
-		cout<<str<<"\t\t\t"<<dist[i]<<endl;
+	return reachableNodes;
+}
+
+// Display all the Reachable Nodes from a node 'n'
+void displayReachableNodes(int n,
+			unordered_map <int, vector<int> > m)
+{
+	vector<int> temp = m[n];
+	for (int i=0; i<temp.size(); i++)
+		cout << temp[i] << " ";
+
+	cout << endl;
+}
+
+// Find all the reachable nodes for every element
+// in the arr
+void findReachableNodes(Graph g, int arr[], int n)
+{
+	// Get the number of nodes in the graph
+	int V = g.V;
+
+	// Take a integer visited array and initialize
+	// all the elements with 0
+	int visited[V+1];
+	memset(visited, 0, sizeof(visited));
+
+	// Map to store list of reachable Nodes for a
+	// given node.
+	unordered_map <int, vector<int> > m;
+
+	// Initialize component Number with 0
+	int componentNum = 0;
+
+	// For each node in arr[] find reachable
+	// Nodes
+	for (int i = 0 ; i < n ; i++)
+	{
+		int u = arr[i];
+
+		// Visit all the nodes of the component
+		if (!visited[u])
+		{
+			componentNum++;
+
+			// Store the reachable Nodes corresponding to
+			// the node 'i'
+			m[visited[u]] = g.BFS(componentNum, u, visited);
+		}
+
+		// At this point, we have all reachable nodes
+		// from u, print them by doing a look up in map m.
+		cout << "Reachable Nodes from " << u <<" are\n";
+		displayReachableNodes(visited[u], m);
 	}
 }
 
+// Driver program to test above functions
 int main()
 {
-	int graph[6][6]={
-		{0, 10, 20, 0, 0, 0},
-		{10, 0, 0, 50, 10, 0},
-		{20, 0, 0, 20, 33, 0},
-		{0, 50, 20, 0, 20, 2},
-		{0, 10, 33, 20, 0, 1},
-		{0, 0, 0, 2, 1, 0}};
-	Dijkstra(graph,0);
+	// Create a graph given in the above diagram
+	int V = 7;
+	Graph g(V);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 4);
+	g.addEdge(3, 1);
+	g.addEdge(5, 6);
+	g.addEdge(5, 7);
+
+	// For every ith element in the arr
+	// find all reachable nodes from query[i]
+	int arr[] = {2, 4, 5};
+
+	// Find number of elements in Set
+	int n = sizeof(arr)/sizeof(int);
+
+	findReachableNodes(g, arr, n);
+
 	return 0;
 }
